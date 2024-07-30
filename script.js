264806bd7fdf8d28ae57b5284682ab6a038a6c7e
TlokotseSM <112977568+TlokotseSM@users.noreply.github.com>
2024-07-30 16:09:32 -0700

    First Commit
    First Date: 30-07-2024

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
